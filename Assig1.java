2d8c9662492365df580c16d34ad4f308a41795e9
G. HAMPI <57933175+hampi-g@users.noreply.github.com>
2020-03-12 18:10:15 +0530

    first program
    day 1 program

---
+++ Assig1.java
@@ -0,0 +1,24 @@
+package com.sonata;
+
+public class Assig1 
+{
+	
+
+		public static void main( String[] args) 
+			
+		{
+			int ar[]= {12,25,89};
+			int max=0;
+			for(int i=0;i<ar.length;i++) 
+			{
+				if(ar[i]>max)
+				{
+					max=ar[i];
+				}
+			
+			}
+			System.out.println("the max number is "+max);
+		}
+
+
+}
