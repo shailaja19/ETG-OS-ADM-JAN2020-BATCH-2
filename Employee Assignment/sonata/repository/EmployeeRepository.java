52730514591e2409a4d7cf8b75d74ca3e343a7f4
G. HAMPI <57933175+hampi-g@users.noreply.github.com>
2020-04-27 22:20:04 +0530

    Employee assignment
    Employee Assignment added

---
+++ Employee Assignment/sonata/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.sonata.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.sonata.Model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
